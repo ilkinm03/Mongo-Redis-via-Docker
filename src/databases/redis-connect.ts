d206686deccc92fe3e1b7391fe2a6047eb9671a3
nickLee <ilkinm2003@gmail.com>
2022-10-08 12:05:10 +0400

    Minor changes

--- src/databases/redis-connect.ts
+++ src/databases/redis-connect.ts
@@ -2,11 +2,12 @@ import Redis from "ioredis";
 import logger from "../logger/logger";
 import envConfig from "../config/env-config";
 
+export const redis = new Redis({
+  host: envConfig.REDIS_HOST,
+});
+
 export const connectRedis = async () => {
   try {
-    const redis = new Redis({
-      host: envConfig.REDIS_HOST,
-    });
     const PONG = await redis.ping();
     logger.info(`Redis is connected! [${PONG}]`);
   } catch (error) {
